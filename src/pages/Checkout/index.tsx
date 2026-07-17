f4535634cd5376e6a2e241974d57379a4729a056
Gideon Yaw Mensa Dappah <gideondmensa@gmail.com>
2026-07-17 20:27:53 +0300

    wip

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,36 @@
+import { useEffect } from 'react'
+import giff from '../../assets/pot.gif'
+import styles from './styles.module.css'
+import { useCart } from '../../context/useCart'
+
+
+const scrollToTop = () => { 
+  window.scrollTo({top:0, behavior:"smooth"})
+}
+
 export default function Checkout() {
-  return <div>Checkout</div>
+  const { clearCart } = useCart() 
+  useEffect(() => { 
+    clearCart()
+    scrollToTop()
+  },[])
+
+  return (
+    <div className={styles.wrapper}>
+      <img src={giff} className={styles.giff}/>
+      <ThankUMessage />
+      <ReceiptMessage />
+  </div>
+  )
+}
+
+const ThankUMessage = () => {
+  return (
+    <h1>תודה שרכשתם SAVOR!</h1>
+  )
+}
+const ReceiptMessage = () => { 
+  return (
+    <div>הקבלה תשלח אליכם בדקות הקרובות למייל.</div>
+  )
 }
